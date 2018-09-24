1c459201e2181ea5274ab498a20433a03420f923
Elif Salman <elifsalman94@hotmail.com>
2018-09-24 07:21:21 +0300

    initial commit

---
+++ app/src/main/cpp/native.cpp
@@ -0,0 +1,401 @@
+#include <royale/CameraManager.hpp>
+#include <royale/ICameraDevice.hpp>
+#include <iostream>
+#include <jni.h>
+#include <android/log.h>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include "opencv2/opencv.hpp"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#define DEBUG
+
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "Native", __VA_ARGS__))
+#define LOGE(...) ((void)__android_log_print(ANDROID_LOG_ERROR, "Native", __VA_ARGS__))
+
+#ifdef DEBUG
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, "Native", __VA_ARGS__))
+#else
+#define LOGD(...)
+#endif
+
+using namespace royale;
+using namespace std;
+using namespace cv;
+
+JavaVM *m_vm;
+jmethodID m_amplitudeCallbackID, m_shapeDetectedCallbackID;
+jobject m_obj;
+
+uint16_t width, height;
+
+enum Mode {UNKNOWN, CAMERA, TEST};
+Mode curmode;
+Mode getType(int i){
+    switch(i)
+    {
+        case 1: return CAMERA;
+        case 2: return TEST;
+        default:
+            LOGE("Unknown mode %d", i);
+            return UNKNOWN;
+    }
+}
+
+const float MAX_DISTANCE = 1.0f;
+
+// this represents the main camera device object
+static std::unique_ptr<ICameraDevice> cameraDevice;
+
+class MyListener : public IDepthDataListener
+{
+    Mat cameraMatrix, distortionCoefficients;
+    Mat zImage, zImage8;
+
+    mutex flagMutex;
+    bool detecting = false;
+    bool detected = false;
+    int count = 0;
+    Mat backgrMat;
+    Mat diff, diffBin;
+    Mat drawing;
+
+    void onNewData (const DepthData *data)
+    {
+        if(detecting)
+        {
+            zImage = Scalar::all (0);
+        }
+        else
+        {
+            zImage = backgrMat.clone();
+        }
+
+        int k = zImage.rows * zImage.cols -1 ; // to flip screen
+        for (int y = 0; y < zImage.rows; y++)
+        {
+            float *zRowPtr = zImage.ptr<float> (y);
+            for (int x = 0; x < zImage.cols; x++, k--)
+            {
+                auto curPoint = data->points.at (k);
+                if (curPoint.depthConfidence > 0)
+                {
+                    zRowPtr[x] = curPoint.z < MAX_DISTANCE ? curPoint.z : MAX_DISTANCE;
+                }
+            }
+        }
+
+        //Background profile
+        if(detecting)
+        {
+            backgrMat += zImage;
+            count++;
+            if(count == 20)
+            {
+                backgrMat /= 20;
+                detecting = false;
+                detected = true;
+                LOGI("Background detecting has ended.");
+            }
+        }
+
+        else if (detected)
+        {
+            diff = backgrMat - zImage;
+            Mat temp = diff.clone();
+            undistort (temp, diff, cameraMatrix, distortionCoefficients);
+
+            boxFilter(diff, diff, -1, Size(5,5));
+            threshold(diff, diffBin, 0.005, 255, CV_THRESH_BINARY);
+            // Find contours
+            diffBin.convertTo(diffBin, CV_8UC1);
+            vector<vector<Point> > contours;
+            findContours(diffBin, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+            if(contours.size() == 0)
+            {
+                LOGD("No contour found");
+            }
+
+            if(curmode == CAMERA){
+                zImage.at<float>(0,0) = MAX_DISTANCE;
+                normalize(zImage, zImage, 0, 255, NORM_MINMAX, CV_8UC1);
+                zImage = 255 - zImage;
+                cvtColor(zImage, drawing, COLOR_GRAY2BGR);
+
+            }
+            for( unsigned int i = 0; i< contours.size(); i++ )
+            {
+                if(contourArea(contours[i])< 50)
+                {
+                    drawContours(drawing, contours,i,Scalar(0,0,255));
+                    continue;
+                }
+
+                Moments mu = moments( contours[i], false);
+                auto center = Point2f( mu.m10/mu.m00 , mu.m01/mu.m00 );
+
+                if(curmode == CAMERA)
+                {
+                    drawContours(drawing, contours,i,Scalar(255,0,0));
+                    circle(drawing, center, 2, Scalar(255,0,0));
+                }
+            }
+        }
+
+        // if it is in camera mode, send debug drawing to java part to show
+        if(curmode == CAMERA)
+        {
+            //  int color = (A & 0xff) << 24 | (R & 0xff) << 16 | (G & 0xff) << 8 | (B & 0xff);
+            jint fill[width * height];
+            k = 0;
+            for (int i = 0; i < drawing.rows; i++)
+            {
+                Vec3b *ptr = drawing.ptr<Vec3b>(i);
+                for (int j = 0; j < drawing.cols; j++)
+                {
+                    Vec3b p = ptr[j];
+                    int color = (255 & 0xff) << 24 | (p[2] & 0xff) << 16 | (p[1] & 0xff) << 8 | (p[0] & 0xff);
+                    fill[k] = color;
+                    k++;
+                }
+            }
+            // attach to the JavaVM thread and get a JNI interface pointer
+            JNIEnv *env;
+            m_vm->AttachCurrentThread((JNIEnv **) &env, NULL);
+            jintArray intArray = env->NewIntArray(width * height);
+            env->SetIntArrayRegion(intArray, 0, width * height, fill);
+            env->CallVoidMethod(m_obj, m_amplitudeCallbackID, intArray);
+            m_vm->DetachCurrentThread();
+        }
+        else if(curmode == TEST) //TEST
+        {
+
+        }
+
+    }
+
+public:
+    void initialize(){
+        zImage.create (Size (width,height), CV_32FC1);
+        backgrMat.create (Size (width,height), CV_32FC1);
+        backgrMat = Scalar::all (0);
+        drawing = Mat::zeros(height, width, CV_8UC3);
+        putText(drawing, "Click Backgr button",Point(30,30),FONT_HERSHEY_PLAIN ,1,Scalar(0,0,255),1);
+    }
+
+    void detectBackground(){
+        LOGI("Background detecting has started.");
+        detecting = true;
+        detected = false;
+        count = 0;
+        backgrMat = Scalar::all (0);
+        drawing = Scalar::all (0);
+        putText(drawing, "Detecting background...",Point(30,30),FONT_HERSHEY_PLAIN ,1,Scalar(0,0,255),1);
+    }
+
+    void setLensParameters (LensParameters lensParameters)
+    {
+        // Construct the camera matrix
+        // (fx   0    cx)
+        // (0    fy   cy)
+        // (0    0    1 )
+        cameraMatrix = (Mat1d (3, 3) << lensParameters.focalLength.first, 0, lensParameters.principalPoint.first,
+                0, lensParameters.focalLength.second, lensParameters.principalPoint.second,
+                0, 0, 1);
+        LOGI("Camera params fx fy cx cy: %f,%f,%f,%f", lensParameters.focalLength.first, lensParameters.focalLength.second,
+             lensParameters.principalPoint.first, lensParameters.principalPoint.second);
+
+        // Construct the distortion coefficients
+        // k1 k2 p1 p2 k3
+        distortionCoefficients = (Mat1d (1, 5) << lensParameters.distortionRadial[0],
+                lensParameters.distortionRadial[1],
+                lensParameters.distortionTangential.first,
+                lensParameters.distortionTangential.second,
+                lensParameters.distortionRadial[2]);
+        LOGI("Dist coeffs k1 k2 p1 p2 k3 : %f,%f,%f,%f,%f", lensParameters.distortionRadial[0],
+             lensParameters.distortionRadial[1],
+             lensParameters.distortionTangential.first,
+             lensParameters.distortionTangential.second,
+             lensParameters.distortionRadial[2]);
+    }
+
+
+};
+
+MyListener listener;
+
+jintArray Java_com_esalman17_embodieddemo_MainActivity_OpenCameraNative (JNIEnv *env, jobject thiz, jint fd, jint vid, jint pid)
+{
+    // the camera manager will query for a connected camera
+    {
+        CameraManager manager;
+
+        auto camlist = manager.getConnectedCameraList (fd, vid, pid);
+        LOGI ("Detected %zu camera(s).", camlist.size());
+
+        if (!camlist.empty())
+        {
+            cameraDevice = manager.createCamera (camlist.at (0));
+        }
+    }
+    // the camera device is now available and CameraManager can be deallocated here
+
+    if (cameraDevice == nullptr)
+    {
+        LOGI ("Cannot create the camera device");
+        jintArray intArray();
+    }
+
+    // IMPORTANT: call the initialize method before working with the camera device
+    CameraStatus ret = cameraDevice->initialize();
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGI ("Cannot initialize the camera device, CODE %d", (int) ret);
+    }
+
+    royale::Vector<royale::String> opModes;
+    royale::String cameraName;
+    royale::String cameraId;
+
+    ret = cameraDevice->getUseCases (opModes);
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGI ("Failed to get use cases, CODE %d", (int) ret);
+    }
+
+    ret = cameraDevice->getMaxSensorWidth (width);
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGI ("Failed to get max sensor width, CODE %d", (int) ret);
+    }
+
+    ret = cameraDevice->getMaxSensorHeight (height);
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGI ("Failed to get max sensor height, CODE %d", (int) ret);
+    }
+
+    ret = cameraDevice->getId (cameraId);
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGI ("Failed to get camera ID, CODE %d", (int) ret);
+    }
+
+    ret = cameraDevice->getCameraName (cameraName);
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGI ("Failed to get camera name, CODE %d", (int) ret);
+    }
+
+    // display some information about the connected camera
+    LOGI ("====================================");
+    LOGI ("        Camera information");
+    LOGI ("====================================");
+    LOGI ("Id:              %s", cameraId.c_str());
+    LOGI ("Type:            %s", cameraName.c_str());
+    LOGI ("Width:           %d", width);
+    LOGI ("Height:          %d", height);
+    LOGI ("Operation modes: %zu", opModes.size());
+
+    for (int i = 0; i < opModes.size(); i++)
+    {
+        LOGI ("    %s", opModes.at (i).c_str());
+    }
+
+    LensParameters lensParams;
+    ret = cameraDevice->getLensParameters (lensParams);
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGE ("Failed to get lens parameters, CODE %d", (int) ret);
+    }else{
+        listener.setLensParameters (lensParams);
+    }
+    listener.initialize();
+
+    // register a data listener
+    ret = cameraDevice->registerDataListener (&listener);
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGI ("Failed to register data listener, CODE %d", (int) ret);
+    }
+
+    // set an operation mode
+    ret = cameraDevice->setUseCase (opModes[0]);
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGI ("Failed to set use case, CODE %d", (int) ret);
+    }
+
+    //set exposure mode to manual
+    ret = cameraDevice->setExposureMode (ExposureMode::MANUAL);
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGE ("Failed to set exposure mode, CODE %d", (int) ret);
+    }
+
+    //set exposure time (not working above 300)
+    ret = cameraDevice->setExposureTime(250);
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGE ("Failed to set exposure time, CODE %d", (int) ret);
+    }
+
+    ret = cameraDevice->startCapture();
+    if (ret != CameraStatus::SUCCESS)
+    {
+        LOGI ("Failed to start capture, CODE %d", (int) ret);
+    }
+
+    jint fill[2];
+    fill[0] = width;
+    fill[1] = height;
+
+    jintArray intArray = env->NewIntArray (2);
+
+    env->SetIntArrayRegion (intArray, 0, 2, fill);
+
+    return intArray;
+}
+
+void Java_com_esalman17_embodieddemo_MainActivity_RegisterCallback (JNIEnv *env, jobject thiz)
+{
+    // save JavaVM globally; needed later to call Java method in the listener
+    env->GetJavaVM (&m_vm);
+
+    m_obj = env->NewGlobalRef (thiz);
+
+    // save refs for callback
+    jclass g_class = env->GetObjectClass (m_obj);
+    if (g_class == NULL)
+    {
+        std::cout << "Failed to find class" << std::endl;
+    }
+
+    // save method ID to call the method later in the listener
+    m_amplitudeCallbackID = env->GetMethodID (g_class, "amplitudeCallback", "([I)V");
+    m_shapeDetectedCallbackID = env->GetMethodID (g_class, "shapeDetectedCallback", "([I)V");
+}
+
+void Java_com_esalman17_embodieddemo_MainActivity_DetectBackgroundNative (JNIEnv *env, jobject thiz)
+{
+    listener.detectBackground();
+}
+
+void Java_com_esalman17_embodieddemo_MainActivity_CloseCameraNative (JNIEnv *env, jobject thiz)
+{
+    cameraDevice->stopCapture();
+}
+
+void Java_com_esalman17_embodieddemo_MainActivity_ChangeModeNative (JNIEnv *env, jobject thiz, jint m)
+{
+    curmode = getType(m);
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ app/src/main/java/com/esalman17/embodieddemo/MainActivity.java
@@ -0,0 +1,303 @@
+package com.esalman17.embodieddemo;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.Point;
+import android.view.Display;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+enum Mode{
+    CAMERA,
+    TEST,
+}
+
+public class MainActivity extends Activity {
+
+    static {
+        System.loadLibrary("usb_android");
+        System.loadLibrary("royale");
+        System.loadLibrary("nativelib");
+    }
+
+    private PendingIntent mUsbPi;
+    private UsbManager manager;
+    private UsbDeviceConnection usbConnection;
+
+    private Bitmap bmpCam = null;
+    private Bitmap bmpTest = null;
+    private ImageView mainImView;
+
+    boolean m_opened;
+    Mode currentMode;
+
+    private static final String LOG_TAG = "MainActivity";
+    private static final String ACTION_USB_PERMISSION = "ACTION_ROYALE_USB_PERMISSION";
+
+    int scaleFactor;
+    int[] resolution;
+    Point displaySize, camRes;
+
+    public native int[] OpenCameraNative(int fd, int vid, int pid);
+    public native void CloseCameraNative();
+    public native void RegisterCallback();
+    public native void DetectBackgroundNative();
+    public native void ChangeModeNative(int mode);
+
+    //broadcast receiver for user usb permission dialog
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                    if (device != null) {
+                        RegisterCallback();
+                        performUsbPermissionCallback(device);
+                        createBitmap();
+                    }
+                } else {
+                    System.out.println("permission denied for device" + device);
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "onCreate()");
+
+        // hide the navigation bar
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        getWindow().getDecorView().setSystemUiVisibility(flags);
+        final View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility)
+            {
+                if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
+                {
+                    decorView.setSystemUiVisibility(flags);
+                }
+            }
+        });
+
+        Button btnCamera= findViewById(R.id.buttonCamera);
+        mainImView =  findViewById(R.id.imageViewMain);
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                openCamera();
+                ChangeModeNative(1);
+                currentMode = Mode.CAMERA;
+            }
+        });
+        findViewById(R.id.buttonBackGr).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!m_opened) {
+                    openCamera();
+                }
+                if(currentMode == null){
+                    ChangeModeNative(1);
+                    currentMode = Mode.CAMERA;
+                }
+                DetectBackgroundNative();
+            }
+        });
+        findViewById(R.id.buttonTest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChangeModeNative(2);
+                currentMode = Mode.TEST;
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        if (m_opened) {
+            CloseCameraNative();
+            m_opened = false;
+        }
+        super.onPause();
+        Log.d(LOG_TAG, "onPause()");
+        unregisterReceiver(mUsbReceiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume()");
+        registerReceiver(mUsbReceiver, new IntentFilter(ACTION_USB_PERMISSION));
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(LOG_TAG, "onDestroy()");
+        //unregisterReceiver(mUsbReceiver);
+
+        if(usbConnection != null) {
+            usbConnection.close();
+        }
+
+        super.onDestroy();
+    }
+
+    public void openCamera() {
+        Log.d(LOG_TAG, "openCamera");
+
+        //check permission and request if not granted yet
+        manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+        if (manager != null) {
+            Log.d(LOG_TAG, "Manager valid");
+        }
+
+        HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
+
+        Log.d(LOG_TAG, "USB Devices : " + deviceList.size());
+
+        Iterator<UsbDevice> iterator = deviceList.values().iterator();
+        UsbDevice device;
+        boolean found = false;
+        while (iterator.hasNext()) {
+            device = iterator.next();
+            if (device.getVendorId() == 0x1C28 ||
+                    device.getVendorId() == 0x058B ||
+                    device.getVendorId() == 0x1f46) {
+                Log.d(LOG_TAG, "royale device found");
+                found = true;
+                if (!manager.hasPermission(device)) {
+                    Intent intent = new Intent(ACTION_USB_PERMISSION);
+                    intent.setAction(ACTION_USB_PERMISSION);
+                    mUsbPi = PendingIntent.getBroadcast(this, 0, intent, 0);
+                    manager.requestPermission(device, mUsbPi);
+                } else {
+                    RegisterCallback();
+                    performUsbPermissionCallback(device);
+                    createBitmap();
+                }
+                break;
+            }
+        }
+        if (!found) {
+            Log.e(LOG_TAG, "No royale device found!!!");
+        }
+    }
+
+    private void performUsbPermissionCallback(UsbDevice device) {
+        usbConnection = manager.openDevice(device);
+        Log.i(LOG_TAG, "permission granted for: " + device.getDeviceName() + ", fileDesc: " + usbConnection.getFileDescriptor());
+
+        int fd = usbConnection.getFileDescriptor();
+
+        resolution = OpenCameraNative(fd, device.getVendorId(), device.getProductId());
+        camRes = new Point(resolution[0], resolution[1]);
+
+        if (resolution[0] > 0) {
+            m_opened = true;
+        }
+    }
+
+    private void createBitmap() {
+
+        // calculate scale factor, which scales the bitmap relative to the display resolution
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        double displayWidth = size.x * 0.9;
+        scaleFactor = (int) displayWidth / resolution[0];
+
+        if (bmpCam == null) {
+            bmpCam = Bitmap.createBitmap(resolution[0], resolution[1], Bitmap.Config.ARGB_8888);
+        }
+    }
+
+    public void amplitudeCallback(int[] amplitudes) {
+        if (!m_opened)
+        {
+            Log.d(LOG_TAG, "Device in Java not initialized");
+            return;
+        }
+        bmpCam.setPixels(amplitudes, 0, resolution[0], 0, 0, resolution[0], resolution[1]);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mainImView.setImageBitmap(bmpCam);
+            }
+        });
+    }
+    // TEST MODE FUNCTIONS ---------------------------------------------------------------------------------
+    private void initializeTestMode(){
+        if (bmpTest == null) {
+            displaySize = new Point();
+            getWindowManager().getDefaultDisplay().getRealSize(displaySize);
+            Log.i(LOG_TAG, "Window display size: x=" + displaySize.x + ", y=" + displaySize.y);
+            bmpTest = Bitmap.createBitmap(displaySize.x, displaySize.y, Bitmap.Config.ARGB_8888);
+        }
+    }
+
+    public void shapeDetectedCallback(int[] descriptors){
+        if (!m_opened)
+        {
+            Log.d(LOG_TAG, "Device in Java not initialized");
+            return;
+        }
+
+    }
+
+    // depends on camera position, not a generic function TODO make generic
+    private Point convertCamPixel2ProPixel(float x, float y, float z){
+        if( x<0 || y<0 || z<=0){
+            return null;
+        }
+        if(displaySize == null || camRes == null){
+            return null;
+        }
+
+        //scale = sin(camFov/2) / sin(proFov/2)
+        double scale_x = 1.3074;
+        double scale_y = 1.8256;
+        double shifty = 486.69004 * Math.exp(-0.048035356*z);
+        double px = x * displaySize.x * scale_x / camRes.x  - displaySize.x*(scale_x -1) /2 ;  // shiftx nearly 0
+        double py = y * displaySize.y * scale_y / camRes.y  - displaySize.y*(scale_y -1)/2 - shifty;
+
+        if(px > displaySize.x || px < 0 || py>displaySize.y || py < 0){
+            Log.d(LOG_TAG, "Point is outside of the projector view");
+            return null;
+        }
+        return  new Point((int)px, (int)py );
+    }
+
+
+}
+
