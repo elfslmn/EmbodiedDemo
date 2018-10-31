d1ae0b6851d6dbe00e5538fe48967d39a892acdc
Elif Salman <elifsalman94@hotmail.com>
2018-10-31 14:13:43 +0300

    add base points and question

--- app/src/main/java/com/esalman17/embodieddemo/Game.java
+++ app/src/main/java/com/esalman17/embodieddemo/Game.java
@@ -4,7 +4,10 @@ import android.graphics.Canvas;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 /**
  * Created by esalman17 on 22.10.2018.
@@ -15,9 +18,30 @@ abstract public class Game {
     protected Rect gesture_left, gesture_right;
     protected Side correctSide;
 
+    protected ArrayList<Point> basePoints = new ArrayList<Point>(12){
+        {
+            add(new Point(-120,-170));
+            add(new Point(-5,-180));
+            add(new Point(115,-180));
+
+            add(new Point(-130,-60));
+            add(new Point(0,-55));
+            add(new Point(130,-65));
+
+            add(new Point(-110,55));
+            add(new Point(5,60));
+            add(new Point(120,55));
+
+            add(new Point(-120,165));
+            add(new Point(-5,180));
+            add(new Point(115,170));
+        }
+    };
+
     public long startTime, assestmentTime; // both in msec
     public GameState state;
     public int level;
+    public Question question;
 
     protected Drawable drawable;
 
@@ -25,6 +49,12 @@ abstract public class Game {
         LEFT,
         RIGHT
     }
+
+    public enum Question{
+        MORE,
+        LESS
+    }
+
     abstract void initialize(int level);
     abstract Canvas initializeCanvas();
     abstract boolean processBlobDescriptors(int[] descriptors);
@@ -49,6 +79,14 @@ abstract public class Game {
         if(correctSide == Side.LEFT) return gesture_right;
         else if(correctSide == Side.RIGHT) return gesture_left;
         else return null;
+    }
+
+    final public int getQuestion(){
+        if(question == Question.MORE) return R.raw.soru_fazla;
+        if(question == Question.LESS) return R.raw.soru_az;
+
+        Log.d("Game", "Question is not specified");
+        return -1;
     }
 
 }
